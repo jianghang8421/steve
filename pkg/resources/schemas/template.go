ef82cf8053bd865a23af5d3bc5ec49473eea504b
Hang <jianghang8421@163.com>
2024-10-29 10:20:15 +0800

    test

--- pkg/resources/schemas/template.go
+++ pkg/resources/schemas/template.go
@@ -48,6 +48,7 @@ type Store struct {
 // Watch will return a APIevent channel that tracks changes to schemas for a user in a given APIRequest.
 // Changes will be returned until Done is closed on the context in the given APIRequest.
 func (s *Store) Watch(apiOp *types.APIRequest, _ *types.APISchema, _ types.WatchRequest) (chan types.APIEvent, error) {
+	logrus.Infof("jianghang template store Watch, apiOp.Schema.ID: %s, apiOp.Schema.Schema.ID: %s", apiOp.Schema.ID, apiOp.Schema.Schema.ID)
 	user, ok := request.UserFrom(apiOp.Request.Context())
 	if !ok {
 		return nil, validation.Unauthorized
@@ -103,6 +104,7 @@ func (s *Store) Watch(apiOp *types.APIRequest, _ *types.APISchema, _ types.Watch
 
 // sendSchemas will send APIEvents onto the provided result channel based on detected changes in the schemas for the provided users.
 func (s *Store) sendSchemas(result chan types.APIEvent, apiOp *types.APIRequest, user user.Info, oldSchemas *types.APISchemas) *types.APISchemas {
+	logrus.Infof("jianghang template sendSchemas, apiOp.Schema.ID: %s, apiOp.Schema.Schema.ID: %s", apiOp.Schema.ID, apiOp.Schema.Schema.ID)
 	// get the current schemas for a user
 	schemas, err := s.sf.Schemas(user)
 	if err != nil {
--- pkg/stores/proxy/proxy_store.go
+++ pkg/stores/proxy/proxy_store.go
@@ -270,6 +270,7 @@ func returnErr(err error, c chan watch.Event) {
 }
 
 func (s *Store) listAndWatch(apiOp *types.APIRequest, client dynamic.ResourceInterface, schema *types.APISchema, w types.WatchRequest, result chan watch.Event) {
+	logrus.Infof("jianghang proxy store listAndWatch, apiOp.Schema.ID: %s", apiOp.Schema.ID)
 	rev := w.Revision
 	if rev == "-1" || rev == "0" {
 		rev = ""
@@ -405,6 +406,7 @@ func (s *Store) watch(apiOp *types.APIRequest, schema *types.APISchema, w types.
 		s.listAndWatch(apiOp, client, schema, w, result)
 		logrus.Debugf("closing watcher for %s", schema.ID)
 		close(result)
+		logrus.Infof("jianghang proxy store close result")
 	}()
 	return result, nil
 }
